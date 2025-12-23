effc82553aae8332617474919a8b2caf1e2db18a
irina <irina@COMPUTER>
2025-12-24 00:04:52 +0500

    ComboBox

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -83,22 +83,16 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		CONST INT SIZE = 256;
 		CHAR sz_buffer[SIZE] = {};
-		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-		SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-		if (HIWORD(wParam) == EN_SETFOCUS)
-		{
-			if (strcmp(sz_buffer, g_sz_INVITE) == 0)
-			{
-				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
-			}
-		}
-		if (HIWORD(wParam) == EN_KILLFOCUS)
-		{
-			if (strcmp(sz_buffer, "") == 0)
-			{
-				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);
-			}
-		}
+		//HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SendMessage((HWND)lParam, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+		if (HIWORD(wParam) == EN_SETFOCUS&& strcmp(sz_buffer, g_sz_INVITE) == 0)
+			
+			SendMessage((HWND)lParam, WM_SETTEXT, 0, (LPARAM)"");
+			
+		
+		if (HIWORD(wParam) == EN_KILLFOCUS&& strcmp(sz_buffer, "") == 0)
+		
+			SendMessage((HWND)lParam, WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);
 	}
 	break;
 	case IDC_BUTTON_COPY:
