d264fece6c8ea6e2d66f0a379ecc2b1d64fd2841
DyadushkinaIS <irinadyadushkina@gmail.com>
2025-12-26 16:10:38 +0500

    ListBox start

---
+++ ListBox/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <Windows.h>
+#include "resource.h"
+
+
+
+CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first","List","Box" };
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM Wparam, LPARAM LParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{	
+		HWND hList = GetDlgItem(hwnd, IDC_LIST);
+		for (int i = 0; i < sizeof(g_sz_VALUES) / sizeof(g_sz_VALUES[0]); i++)
+		{
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)g_sz_VALUES[i]);
+		}
+	}
+	break;
+
+	case WM_COMMAND:
+		break;
+
+	case WM_CLOSE:		
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
