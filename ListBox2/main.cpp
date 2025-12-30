3fbb4109f8ebca3cc6da48dc9dc2cce11215535d
DyadushkinaIS <irinadyadushkina@gmail.com>
2025-12-30 16:27:23 +0500

    ListBox2 Start

---
+++ ListBox2/main.cpp
@@ -0,0 +1,45 @@
+#undef UNICODE
+#include <Windows.h>
+#include "resource.h"
+CONST CHAR* g_sz_VALUES[] = { "This","is","my","first","List","Box" };
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG_MAIN), NULL, DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hList = GetDlgItem(hwnd, IDC_LIST);
+		for (int i = 0; i < sizeof(g_sz_VALUES) / sizeof(g_sz_VALUES[0]); i++)
+		{
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)g_sz_VALUES[i]);
+		}
+	}
+		break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_ADD:
+		{
+
+		}
+		break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+		}
+	}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
---
+++ ListBox2/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Resource.rc
+//
+#define IDD_DIALOG_MAIN                 101
+#define IDD_DIALOG_ADD                  103
+#define IDC_LIST                        1001
+#define IDC_BUTTON_ADD                  1004
+#define IDC_BUTTON_DELETE               1005
+#define IDC_EDIT1                       1006
+#define IDC_EDIT_ADD                    1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
