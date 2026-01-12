e42800cdce9ef25daa85345f7ccc20bc811b4ae6
DyadushkinaIS <irinadyadushkina@gmail.com>
2026-01-12 14:49:56 +0500

    Calc

---
+++ Calc/main.cpp
@@ -0,0 +1,79 @@
+#undef UNICODE
+#include <Windows.h>
+
+CONST CHAR g_sz_WINDOW_CLASS[] = "Calc PV_522";
+LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//1) Регистрация класса окна: 
+	WNDCLASSEX wClass;
+	ZeroMemory(&wClass, sizeof(wClass));
+
+	wClass.style = NULL;
+	wClass.cbSize = sizeof(wClass);
+	wClass.cbClsExtra = 0;
+	wClass.cbWndExtra = 0;
+
+	wClass.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wClass.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);
+	wClass.hCursor = LoadCursor(hInstance, IDC_ARROW);
+	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+
+	wClass.hInstance = hInstance;
+	wClass.lpszClassName = g_sz_WINDOW_CLASS;
+	wClass.lpszMenuName = NULL;
+	wClass.lpfnWndProc = (WNDPROC)WndProc;
+
+	if (!RegisterClassEx(&wClass))
+	{
+		MessageBox(NULL, "Class registration failed", NULL, MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	// 2) Создание окна:
+	HWND hwnd = CreateWindowEx
+	(
+		NULL,
+		g_sz_WINDOW_CLASS,
+		g_sz_WINDOW_CLASS,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	// 3) Запуск цикла сообщений:	
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+	default:return DefWindowProc(hwnd,uMsg,lParam,wParam);
+	}
+	return FALSE;
+
+}
