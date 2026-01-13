7b213dd6a557db45d1374653a692ce84b801a4c1
irina <irina@COMPUTER>
2026-01-13 18:03:08 +0500

    Buttons DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -2,6 +2,22 @@
 #include <Windows.h>
 #include "resource.h"
 
+#define g_i_BUTTON_SIZE					50
+#define g_i_INTERVAL					2
+#define g_i_DOUBLE_BUTTON_SIZE			g_i_BUTTON_SIZE*2+g_i_INTERVAL
+#define g_i_START_X						10
+#define g_i_START_Y						10
+#define g_i_DISPLAY_WIDTH				g_i_BUTTON_SIZE*5+g_i_INTERVAL*4
+#define g_i_DISPLAY_HEIGHT				22
+#define g_i_BUTTON_START_X				g_i_START_X
+#define g_i_BUTTON_START_Y				g_i_START_Y+g_i_DISPLAY_HEIGHT+g_i_INTERVAL
+
+#define BUTTON_X_POSITION(SHIFT)		g_i_BUTTON_START_X+(g_i_BUTTON_SIZE+g_i_INTERVAL)*(SHIFT)	
+#define BUTTON_Y_POSITION(SHIFT)		g_i_BUTTON_START_Y+(g_i_BUTTON_SIZE+g_i_INTERVAL)*(SHIFT)
+
+CONST CHAR g_OPERATIONS[] = { "+-*/" };
+
+
 CONST CHAR g_sz_WINDOW_CLASS[] = "Calc PV_522";
 LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -68,12 +84,108 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HWND hEdit = CreateWindowEx
 		(
-			NULL, "Edit",0,
-			WS_CHILD|WS_VISIBLE,
-			10,10,
-			400,22,
+			NULL, "Edit", "0",
+			WS_CHILD|WS_VISIBLE|WS_BORDER|ES_RIGHT,
+			g_i_START_X, g_i_START_Y,
+			g_i_DISPLAY_WIDTH, g_i_DISPLAY_HEIGHT,
 			hwnd,
 			(HMENU)IDC_DISPLAY,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		/////////////////////////////////////////////////////////////////////////////////
+		CHAR sz_digit[2] = {};
+		for (int i = 6; i >= 0; i -= 3)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				sz_digit[0] = i + j + '1';
+				CreateWindowEx
+				(
+					NULL,"Button",sz_digit,
+					WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON,
+					BUTTON_X_POSITION(j),BUTTON_Y_POSITION(2-i/3),
+					//g_i_BUTTON_START_X+(g_i_BUTTON_SIZE+g_i_INTERVAL)*j,		  //X - position
+					//g_i_BUTTON_START_Y+(g_i_BUTTON_SIZE+g_i_INTERVAL)*(2-i/3),  //Y-position
+					g_i_BUTTON_SIZE,g_i_BUTTON_SIZE,
+					hwnd,
+					(HMENU)(IDC_BUTTON_1+i+j),
+					GetModuleHandle(NULL),
+					NULL
+				);
+			}
+		}
+		/////////////////////////////////////////////////////////////////////////////////
+		CreateWindowEx
+		(
+			NULL,"Button","0",
+			WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON,
+			BUTTON_X_POSITION(0), BUTTON_Y_POSITION(3),
+			g_i_DOUBLE_BUTTON_SIZE,g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)(IDC_BUTTON_0),
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL,"Button",".",
+			WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON,
+			BUTTON_X_POSITION(2),BUTTON_Y_POSITION(3),
+			g_i_BUTTON_SIZE,g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_POINT,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		/////////////////////////////////////////////////////////////////////////////////
+		CHAR sz_operation[2] = {};
+		for (int i = 0; i < 4; i++)
+		{
+			sz_operation[0] = g_OPERATIONS[3 - i];
+			CreateWindowEx
+			(
+				NULL,"Button",sz_operation,
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+				BUTTON_X_POSITION(3),BUTTON_Y_POSITION(i),
+				g_i_BUTTON_SIZE,g_i_BUTTON_SIZE,
+				hwnd,
+				HMENU(IDC_BUTTON_SLASH-i),
+				GetModuleHandle(NULL),
+				NULL
+			);
+		}
+		/////////////////////////////////////////////////////////////////////////////////
+		CreateWindowEx
+		(
+			NULL,"Button","<-",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_X_POSITION(4),BUTTON_Y_POSITION(0),
+			g_i_BUTTON_SIZE,g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_BSP,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "Button", "C",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_X_POSITION(4), BUTTON_Y_POSITION(1),
+			g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_CLR,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "Button", "=",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_X_POSITION(4), BUTTON_Y_POSITION(2),
+			g_i_BUTTON_SIZE, g_i_DOUBLE_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_EQUAL,
 			GetModuleHandle(NULL),
 			NULL
 		);
@@ -89,5 +201,4 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	default:return DefWindowProc(hwnd,uMsg,wParam,lParam);
 	}
 	return FALSE;
-
 }
